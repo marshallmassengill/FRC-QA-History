3fcc373e65831aeb08391b970ddccc0ffb070afd
Marshall <marshallmassengill@gmail.com>
2019-10-05 16:35:01 +0000

    daily commit

--- itGitsTheQA.sh
+++ itGitsTheQA.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-touch /tmp/test1
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 curl https://frc-qa.firstinspires.org/onepage.html > ./onepage.html
 git add .
 git commit -m "daily commit"
 git push
-touch /tmp/test2
